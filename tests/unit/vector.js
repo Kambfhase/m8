436e18d98782d019e4a3f53513fac0c50b098baa
Kambfhase <kambfhase@hotmail.de>
2010-06-27 13:50:18 +0200

    improved vector unit tests

--- tests/unit/vector.js
+++ tests/unit/vector.js
@@ -18,11 +18,13 @@ YUI({
             Y.Assert.isTrue( Vector.is( Vector( [[ 1]])));
             Y.Assert.isTrue( Vector.is( Vector( 3, 2)));
             Y.Assert.isFalse( Vector.is( [[ 1]]));
+            Y.Assert.isInstanceOf( Vector, Vector([[1],[2],[3]]));
             Y.ArrayAssert.itemsAreEqual2D( [[1]], Vector([[ 1]]));
             Y.ArrayAssert.itemsAreEqual2D( [[2],[2],[2],[2],[2]], Vector( 5, 2));
             // Vector.create()
             Y.Assert.isTrue( Vector.is( Vector.create( [[ 1]])));
             Y.Assert.isTrue( Vector.is( Vector.create( 3, 2)));
+            Y.Assert.isInstanceOf( Vector, Vector.create([1,2,3,4]));
             Y.ArrayAssert.itemsAreEqual2D( [[1]], Vector.create([[ 1]]));
             Y.ArrayAssert.itemsAreEqual2D( [[1]], Vector.create([ 1]));
             Y.ArrayAssert.itemsAreEqual2D( [[1],[2],[3],[4],[5],[6],[7],[8],[9]], Vector([1,2,3,4,5,6,7,8,9]));
@@ -67,28 +69,32 @@ YUI({
             
             Y.Assert.isTrue( Vector.is( Vector([[1]]).add([[1]])));
             Y.Assert.isTrue( Vector([[2]]).add([[-1]]).equals([[1]]));
+            Y.ArrayAssert.itemsAreEqual2D( [[5,5,5,5,5]], Vector([[1,2,3,4,5]]).add([[4,3,2,1,0]]));
+            Y.ArrayAssert.itemsAreEqual2D( [[5],[5],[5],[5],[5]], Vector([1,2,3,4,5]).add([[4],[3],[2],[1],[0]]));
         },
         
         testDot: function(){
             Y.Assert.isFunction( Vector.prototype.dot);
             
             Y.Assert.isNumber( Vector([[1]]).dot([[1]]));
-            Y.Assert.areEqual( Vector([[1]]).dot([[1]]), 1);
-            Y.Assert.areEqual( Vector( 20, 1).dot( Vector( 20,1)), 20);
-            Y.Assert.areEqual( Vector([[1],[2],[3]]).dot([[6],[3],[2]]), 18);
+            Y.Assert.areSame( 1, Vector([[1]]).dot([[1]]));
+            Y.Assert.areSame( 20, Vector( 20, 1).dot( Vector( 20,1)));
+            Y.Assert.areSame( 18, Vector([[1],[2],[3]]).dot([[6],[3],[2]]));
+            Y.Assert.areSame( 3, Vector([1,1,1]).dot([[1,1,1]]), "The dot product should be independent of the vectors orientation.");
+            Y.Assert.areSame( 6, Vector([[1,1,1]]).dot([[1,2,3]]));
         },
         
         testMagnitude: function(){
             Y.Assert.isFunction( Vector.prototype.magnitude);
             
             Y.Assert.isNumber( Vector([1,2,3,4]).magnitude());
-            Y.Assert.areSame( Vector([0,0,0,0]).magnitude(),0);
-            Y.Assert.areSame( Vector([[0,0,0,0]]).magnitude(),0);
-            Y.Assert.areSame( Vector([0,0,0,1]).magnitude(), 1);
-            Y.Assert.areSame( Vector([[0,0,0,1]]).magnitude(), 1);
-            Y.Assert.areSame( Vector([[3],[4]]).magnitude(), 5);
-            Y.Assert.areSame( Vector([-3 ,-4] ).magnitude(), 5);
-            Y.Assert.areSame( Vector(2,1).magnitude(), Math.pow( 2, .5));
+            Y.Assert.areSame( 0, Vector([0,0,0,0]).magnitude());
+            Y.Assert.areSame( 0, Vector([[0,0,0,0]]).magnitude());
+            Y.Assert.areSame( 1, Vector([0,0,0,1]).magnitude());
+            Y.Assert.areSame( 1, Vector([[0,0,0,1]]).magnitude());
+            Y.Assert.areSame( 5, Vector([[3],[4]]).magnitude());
+            Y.Assert.areSame( 5, Vector([-3 ,-4] ).magnitude());
+            Y.Assert.areSame( Math.SQRT2, Vector(2,1).magnitude());
         },
         
         testNormalize: function(){
@@ -100,6 +106,20 @@ YUI({
             Y.Assert.isTrue( Vector(2,1).normalize().equals([[ Math.pow( 2,.5)/2],[Math.SQRT2/2]]));
             Y.Assert.isTrue( Math.abs( Vector([1,2,3,4]).normalize().magnitude() - 1) < Vector.precision);
             Y.Assert.isTrue( Math.abs( Vector([12,-456,486,0,234.412]).normalize().magnitude() - 1) < Vector.precision);
+        },
+        
+        testCoords: function(){
+            Y.Assert.isFunction( Vector.prototype.coords);
+            
+            Y.Assert.isInstanceOf( Vector, Vector([1,2,3,4]).coords());
+            Y.Assert.isTrue( Vector.is( Vector([1,2,3,4]).coords()));
+            var a = [[1],[2],[3],[4]];
+            Y.ArrayAssert.itemsAreEqual2D( a, Vector(a).coords());
+            Y.ArrayAssert.itemsAreEqual2D( a, Vector(a).coords().coords());
+            a=Vector(a);
+            Y.ArrayAssert.itemsAreEqual2D( a, a.coords());
+            Y.Assert.areNotSame( a, a.coords());
+            Y.ArrayAssert.itemsAreEqual2D( a, Vector([[1,2,3,4]]).coords());
         }
     });
     
